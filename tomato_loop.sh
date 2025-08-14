e88a1c59bba0a0531867b790664d9f5587aab58c
francesco <francesco.betti.sorbelli@gmail.com>
2025-08-14 11:58:49 +0200

    Make capture interval configurable in tomato_loop.sh
    Previously, the script ran at a fixed 2-minute interval. Added a `FREQ` variable to allow customizing the interval while maintaining the default behavior. Updated the condition logic to use this configurable interval.

--- tomato_loop.sh
+++ tomato_loop.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
-# Tomato cam loop: 07:00..20:00 inclusive, every 2 minutes
+# Tomato cam loop: 07:00..20:00 inclusive, configurable interval
 
 PYTHON="/home/fra/pyenv/bin/python"
 CLIENT="/home/fra/rpi-cam/client.py"
 FLOCK="/usr/bin/flock"
 LOCK="/tmp/tomato-cam.lock"
 
+FREQ=2   # interval in minutes (e.g., 2, 5, 10)
+
 while true; do
     HOUR=$(date +%H)
     MIN=$(date +%M)
 
-    # Run only between 07:00 and 20:00, at multiples of 2 minutes (:00, :02, :04, ...)
-    if [[ $HOUR -ge 7 && $HOUR -le 20 && $((10#$MIN % 2)) -eq 0 ]]; then
+    # Run only between 07:00 and 20:00 at multiples of $FREQ minutes
+    if [[ $HOUR -ge 7 && $HOUR -le 20 && $((10#$MIN % FREQ)) -eq 0 ]]; then
         $FLOCK -n "$LOCK" "$PYTHON" "$CLIENT" photo
         sleep 60   # avoid multiple runs within the same minute
     else
